1124166b505f95d973cc2405b59e0427da0f0509
Axa Cheng <axa@axa-macbookair.local>
2012-07-29 16:46:48 +0800

    Add setup_sublime.sh for adding SublimeGit and SublimeLinter

---
+++ src/tools/setup_sublime.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# PLEASE run this script by using:
+# . setup_sublime.sh 
+# Instead of ./setup_sublime.sh
+# Because CWD issue can not allow you change directory!
+
+SUBLIME_HOME="$HOME"/Library/Application\ Support/Sublime\ Text\ 2
+#SUBLIME_HOME='/tmp/aaa'
+
+
+### Setup Git ###
+cd "$SUBLIME_HOME"/Packages/
+git clone git://github.com/kemayo/sublime-text-2-git.git Git
+
+
+# Changing git_command path
+cp "$SUBLIME_HOME"/Packages/Git/Git.sublime-settings  "$SUBLIME_HOME"/Packages/User/
+perl -i -ple 's/,"git_command": false/,"git_command": "\/usr\/local\/git\/bin\/git"/g if $. == 7; close ARGV if eof' "$SUBLIME_HOME"/Packages/User/Git.sublime-settings
+
+
+### Setup SublimeLinter ###
+echo ""
+cd "$SUBLIME_HOME"/Packages/
+git clone https://github.com/SublimeLinter/SublimeLinter.git
+
+echo ""
+echo "Setup is completed. Enjoy~~~"
+echo "You can use Command+Shift+p to call git comamnd set"
+echo "You can also setup SublimeLinter by going to Perference->Package Settings->SublimeLinter" 
